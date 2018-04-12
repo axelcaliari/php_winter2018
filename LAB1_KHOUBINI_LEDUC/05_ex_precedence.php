cc4299e1761781d5d02435c68aaf47a11e18e009
Andrew Caya <andrewscaya@yahoo.ca>
2018-04-12 02:28:34 -0400

    2018-04-12 AC: Lab 1 Khoubini Leduc.

--- LAB1_KHOUBINI_LEDUC/05_ex_precedence.php
+++ LAB1_KHOUBINI_LEDUC/05_ex_precedence.php
@@ -8,4 +8,6 @@ echo "10" + "0.6". 44 . 1 . PHP_EOL;
  * Please explain:
  *
  * We got 10.6441 from the operation  between 10 and 0.6441 that has been concatenated
+ *
+ * The comma separates echo's parameters and acts as a concatenation operator in this context.
  */
